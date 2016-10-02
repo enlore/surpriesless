6657a76ed1f88d56c2ede69db9546b38f8497d94
Adia Tigney <adiatigney@gmail.com>
2016-10-01 20:39:33 -0500

    updated port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + "/static"))
 // see a list of items that hav been listed
 // GET - list all items
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.info(`Listening on ${port}`)
