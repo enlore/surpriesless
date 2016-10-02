cbaa843db0af49f0b64dd36c108f9ef6f4a2d75e
enlore <n.e.lorenson@gmail.com>
2016-10-01 19:47:44 -0500

    Purchase GET route

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const app = express()
 
 app.use(express.static(__dirname + "/static"))
 
+// submit sale, submit purchase
+// GET - submit sale form
+// user fills out form
+// POST - submit sales form
+// send crap to blockchain
+// save item for sale in local db, id of such
+//
+// see a list of items that hav been listed
+// GET - list all items
+
+app.get("/submit-purchase", function (req, res, next) {
+    res.send("Hey you asked for " + req.url)
+})
+
 const port = process.env.PORT
 
 app.listen(port, () => {
