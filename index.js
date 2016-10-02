e70f3b2a9955fce4e6a0d9d6627be5954a6b1271
enlore <n.e.lorenson@gmail.com>
2016-10-01 19:52:21 -0500

    Just serve a static form

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ app.use(express.static(__dirname + "/static"))
 // see a list of items that hav been listed
 // GET - list all items
 
-app.get("/submit-purchase", function (req, res, next) {
-    res.send("Hey you asked for " + req.url)
-})
-
 const port = process.env.PORT
 
 app.listen(port, () => {
