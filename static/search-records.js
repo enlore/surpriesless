7b14bbf5fee75347599e4c72ee04e8f4e8ebeb59
enlore <n.e.lorenson@gmail.com>
2016-10-02 09:47:47 -0500

    Basic record filter

--- static/search-records.js
+++ static/search-records.js
@@ -3,7 +3,37 @@
         el: "#records-view",
 
         data: {
-            records: []
+            query: "",
+            _records: []
+        },
+
+        computed: {
+            records: {
+                set: function (val) {
+                    console.info("set", val)
+                    this._records = val
+                },
+
+                get: function () {
+                    var query = this.query.trim()
+
+                    console.info("get", query)
+
+                    if (query === "" || query === void 0)
+                        return this._records
+                    else {
+                        return this._records.filter(function (record) {
+                            return record.id === query
+                        })
+                    }
+                }
+            }
+        },
+
+        methods: {
+            doSearch: function doSearch () {
+                var self = this
+            },
         },
 
         created: function () {
@@ -11,7 +41,10 @@
 
             superagent.get("/purchase-history")
                 .end(function (err, resp) {
+                    if (err) console.error(err)
                     self.records = resp.body.records
+                    self.query = "x"
+                    self.query = "" // HAX
                 })
         }
     })
